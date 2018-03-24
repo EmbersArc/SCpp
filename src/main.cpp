abbca1df6b9c049ca850858cb4ff17d58b425df6
EmbersArc <s-niederberger@hotmail.com>
2018-03-24 13:15:34 +0100

    optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ BMatrix B;
 fVector f;
 
 typedef Eigen::Matrix<double,14,23> state_type;
+
 class ode_dVdt{
 private:
     Vector3d u_t, u_t1;
@@ -33,7 +34,7 @@ public:
         Phi_A_xi = V.block<14, 14>(0, 1).inverse();
 
         dVdt.block<14, 1>(0, 0) = sigma * f();
-        dVdt.block<14, 14>(0, 1) = A()* Phi_A_xi;
+        dVdt.block<14, 14>(0, 1) = A()* V.block<14, 14>(0, 1);
         dVdt.block<14, 3>(0, 15) = Phi_A_xi * B() * alpha;
         dVdt.block<14, 3>(0, 18) = Phi_A_xi * B() * beta;
         dVdt.block<14, 1>(0, 21) = Phi_A_xi * f();
@@ -96,7 +97,7 @@ int main(){
             dVdt.Update(U.col(k), U.col(k+1), sigma);
             integrate_adaptive(make_controlled(1E-12 , 1E-12 , stepper), dVdt, V, 0., dt, dt/10.);
 
-            A_bar[k] = V.block<14,14>(0, 1) * V.block<14,14>(0, 1);
+            A_bar[k] = V.block<14,14>(0, 1);
             B_bar[k] = V.block<14,14>(0, 1) * V.block<14,3>(0, 15);
             C_bar[k] = V.block<14,14>(0, 1) * V.block<14,3>(0, 18);
             Sigma_bar[k] = V.block<14,14>(0, 1) * V.block<14,1>(0, 21);
