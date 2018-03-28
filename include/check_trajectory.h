dd412ac394558093ee9b45638acaa8801f3f740e
EmbersArc <s-niederberger@hotmail.com>
2018-03-28 23:19:50 +0200

    initial commit, probably wrong

---
+++ include/check_trajectory.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <boost/numeric/odeint.hpp>
+#include <boost/numeric/odeint/external/eigen/eigen_algebra.hpp>
+
+#include "active_model.hpp"
+
+class ODE{
+
+private:
+    Model model;
+    Model::ControlVector U0;
+    Model::ControlVector U1;
+
+public:
+    void operator()(const Model::StateVector &X, Model::StateVector &dXdt, double t);
+    void update_input(const Model::ControlVector &U0, const Model::ControlVector &U1);
+
+};
+
+template<int K>
+void check_trajectory(
+        Eigen::Matrix<double, Model::n_states, K> &X_in,
+        Eigen::Matrix<double, Model::n_inputs, K> &U,
+        double sigma
+) {
+    using namespace boost::numeric::odeint;
+
+    MatrixXd X_exact(Model::n_states, K);
+    X_exact.col(0) = X_in.col(0);
+
+    Model::StateVector x = X_in.col(0);
+
+    runge_kutta_dopri5<Model::StateVector , double, Model::StateVector , double, vector_space_algebra> stepper;
+
+    ODE ode;
+
+    double dt = sigma / (K - 1);
+
+    for(int k = 1; k < K; k++){
+        ode.update_input(U.col(k-1), U.col(k));
+        integrate_adaptive(make_controlled(1e-12, 1e-12, stepper), ode, x, 0., dt, dt/50);
+        X_exact.col(k) = x;
+    }
+
+    double total_absolute_error = (X_exact - X_in).cwiseAbs().sum();
+
+    std::cout << X_exact - X_in << std::endl << std::endl;
+
+    std::cout << std::endl << "Total absolute error in trajectory: " << total_absolute_error << std::endl << std::endl;
+
+};
---
+++ src/check_trajectory.cpp
@@ -0,0 +1,13 @@
+#include "check_trajectory.h"
+
+
+void ODE::operator()(const Model::StateVector &X, Model::StateVector &dXdt, const double t){
+    Model::ControlVector U = U0 + t * (U1 - U0);
+    dXdt = model.ode(X,U);
+};
+
+void ODE::update_input(const Model::ControlVector &U0, const Model::ControlVector &U1) {
+    this->U0 = U0;
+    this->U1 = U1;
+};
+
