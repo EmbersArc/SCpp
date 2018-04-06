0b86407f3b42d8579852d927f8cd712f1c0989ab
EmbersArc <s-niederberger@hotmail.com>
2018-04-06 22:36:31 +0200

    move function body to src

--- src/check_trajectory.cpp
+++ src/check_trajectory.cpp
@@ -11,3 +11,34 @@ void ODE::update_input(const Model::ControlVector &U0, const Model::ControlVecto
     this->U1 = U1;
 }
 
+void check_trajectory(
+        Eigen::Matrix<double, Model::n_states, K> &X_in,
+        Eigen::Matrix<double, Model::n_inputs, K> &U,
+        double sigma
+) {
+    using namespace boost::numeric::odeint;
+
+    MatrixXd X_exact(Model::n_states, K);
+    X_exact.col(0) = X_in.col(0);
+
+    Model::StateVector x = X_in.col(0);
+
+    runge_kutta_dopri5<Model::StateVector , double, Model::StateVector , double, vector_space_algebra> stepper;
+
+
+    double dt = sigma / (K - 1);
+    ODE ode(dt);
+
+    for(int k = 1; k < K; k++){
+        ode.update_input(U.col(k-1), U.col(k));
+        integrate_adaptive(make_controlled(1e-12, 1e-12, stepper), ode, x, 0., dt, dt/15);
+        X_exact.col(k) = x;
+    }
+
+    double total_absolute_error = (X_exact - X_in).cwiseAbs().sum();
+    std::cout << std::endl;
+    std::cout << "Absolute errors: " << std::endl;
+    std::cout << X_exact - X_in << std::endl;
+    std::cout << std::endl << "Total absolute error: " << total_absolute_error << std::endl << std::endl;
+
+}
