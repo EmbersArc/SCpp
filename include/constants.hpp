821fcf1f1093c62492415c14ef36711e6b69cba4
Janis Maczijewski <janismac@gmx.de>
2018-04-08 22:19:28 +0200

    refactoring, extract discretization

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,5 +2,5 @@
 
 
 // Number of discretization steps
-#define K (50)
+#define K (20)
 #define PI (3.14159265358979323846)
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "active_model.hpp"
 #include "EcosWrapper.hpp"
 #include "MosekWrapper.hpp"
+#include "Discretization.hpp"
 
 #include <iostream>
 #include <array>
@@ -32,57 +33,6 @@ using std::setw;
 using std::setfill;
 
 
-class DiscretizationODE {
-private:
-    Model::ControlVector u_t, u_t1;
-    double sigma, dt;
-    Model& model;
-
-public:
-
-    static constexpr size_t n_V_states = 3 + Model::n_states + 2 * Model::n_inputs;
-    using state_type = Eigen::Matrix<double, Model::n_states, n_V_states>;
-
-    DiscretizationODE(
-        const Model::ControlVector &u_t, 
-        const Model::ControlVector &u_t1, 
-        const double &sigma, 
-        double dt,
-        Model& model
-    )
-    :u_t(u_t)
-    ,u_t1(u_t1)
-    ,sigma(sigma)
-    ,dt(dt)
-    ,model(model) {}
-
-    void operator()(const state_type &V, state_type &dVdt, const double t){
-
-        const Model::StateVector &x = V.col(0);
-        const Model::ControlVector u = u_t + t / dt * (u_t1 - u_t);
-
-        const double alpha = t / dt;
-        const double beta = 1. - alpha;
-
-        const Model::StateMatrix   A_bar  = sigma * model.state_jacobian(x, u);
-        const Model::ControlMatrix B_bar  = sigma * model.control_jacobian(x, u);
-        const Model::StateVector   f      =         model.ode(x, u);
-
-
-        Model::StateMatrix Phi_A_xi = V.block<Model::n_states, Model::n_states>(0, 1);
-        Model::StateMatrix Phi_A_xi_inverse = Phi_A_xi.inverse();
-
-        size_t cols = 0;
-
-        dVdt.block<Model::n_states,               1>(0, cols) = sigma * f;                                   cols += 1;
-        dVdt.block<Model::n_states, Model::n_states>(0, cols) = A_bar * Phi_A_xi;                            cols += Model::n_states;
-        dVdt.block<Model::n_states, Model::n_inputs>(0, cols) = Phi_A_xi_inverse * B_bar * alpha;            cols += Model::n_inputs;
-        dVdt.block<Model::n_states, Model::n_inputs>(0, cols) = Phi_A_xi_inverse * B_bar * beta;             cols += Model::n_inputs;
-        dVdt.block<Model::n_states,               1>(0, cols) = Phi_A_xi_inverse * f;                        cols += 1;
-        dVdt.block<Model::n_states,               1>(0, cols) = Phi_A_xi_inverse * (-A_bar * x - B_bar * u);
-
-    }
-};
 
 string get_output_path() {
     return "../output/" + Model::get_name() + "/";
@@ -346,9 +296,6 @@ int main() {
     //EcosWrapper solver(socp);
     MosekWrapper solver(socp);
 
-    using namespace boost::numeric::odeint;
-    runge_kutta4<DiscretizationODE::state_type, double, DiscretizationODE::state_type, double, vector_space_algebra> stepper;
-
     const size_t iterations = 40;
     for(size_t it = 0; it < iterations; it++) {
         clock_t begin_time = clock();
@@ -359,34 +306,9 @@ int main() {
         cout << "Calculating new transition matrices." << endl;
 
 
-        for (size_t k = 0; k < K-1; k++) {
-            DiscretizationODE::state_type V;
-            V.setZero();
-            V.col(0) = X.col(k);
-            V.block<Model::n_states,Model::n_states>(0, 1).setIdentity();
 
-            DiscretizationODE discretizationODE(U.col(k), U.col(k+1), sigma, dt, model);
-            integrate_n_steps( stepper , discretizationODE , V , 0. , dt/10.0 , 10 );
+        calculate_discretization ( model, sigma, X, U, A_bar, B_bar, C_bar, Sigma_bar, z_bar );
 
-            size_t cols = 1;
-            A_bar[k]      =            V.block<Model::n_states,Model::n_states>(0, cols);   cols += Model::n_states;
-            B_bar[k]      = A_bar[k] * V.block<Model::n_states,Model::n_inputs>(0, cols);   cols += Model::n_inputs;
-            C_bar[k]      = A_bar[k] * V.block<Model::n_states,Model::n_inputs>(0, cols);   cols += Model::n_inputs;
-            Sigma_bar[k]  = A_bar[k] * V.block<Model::n_states,1>(0, cols);                 cols += 1;
-            z_bar[k]      = A_bar[k] * V.block<Model::n_states,1>(0, cols);
-
-            /*cout << "A_bar " << k << endl;
-            cout << A_bar[k] << endl << endl;
-            cout << "B_bar " << k << endl;
-            cout << B_bar[k] << endl << endl;
-            cout << "C_bar " << k << endl;
-            cout << C_bar[k] << endl << endl;
-            cout << "Sigma_bar " << k << endl;
-            cout << Sigma_bar[k] << endl << endl;
-            cout << "z_bar " << k << endl;
-            cout << z_bar[k] << endl << endl;*/
-        }
-        
 
         // Write problem to file
 
