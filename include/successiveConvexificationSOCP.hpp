76a0f588f6db99ad03f7cb91d4493bcef781807a
Sven Niederberger <s-niederberger@hotmail.com>
2019-01-25 15:54:33 +0100

    new signature

--- include/successiveConvexificationSOCP.hpp
+++ include/successiveConvexificationSOCP.hpp
@@ -3,22 +3,20 @@
 #include "optimizationProblem.hpp"
 #include "active_model.hpp"
 
-using std::array;
-
 namespace sc
 {
 op::SecondOrderConeProgram build_sc_SOCP(
     Model &model,
-    double &weight_trust_region_sigma,
-    Model::state_vector_t &weight_trust_region_x,
-    Model::input_vector_t &weight_trust_region_u,
-    Model::state_vector_t &weight_virtual_control,
+    double &weight_trust_region_time,
+    double &weight_trust_region_state,
+    double &weight_virtual_control,
     Eigen::MatrixXd &X,
     Eigen::MatrixXd &U,
     double &sigma,
-    vector<Model::state_matrix_t> &A_bar,
-    vector<Model::control_matrix_t> &B_bar,
-    vector<Model::control_matrix_t> &C_bar,
-    vector<Model::state_vector_t> &Sigma_bar,
-    vector<Model::state_vector_t> &z_bar);
+    Model::state_matrix_v_t &A_bar,
+    Model::control_matrix_v_t &B_bar,
+    Model::control_matrix_v_t &C_bar,
+    Model::state_vector_v_t &Sigma_bar,
+    Model::state_vector_v_t &z_bar);
 }
+
