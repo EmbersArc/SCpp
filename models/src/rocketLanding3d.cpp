a58dabdc8fb915cf3cbeba4a81ae69990f3ac9a7
Sven Niederberger <s-niederberger@hotmail.com>
2019-02-19 18:47:19 +0100

    move definition to source

--- models/src/rocketLanding3d.cpp
+++ models/src/rocketLanding3d.cpp
@@ -12,11 +12,10 @@ RocketLanding3D::RocketLanding3D()
     p.loadFromFile();
 }
 
-void RocketLanding3D::systemFlowMap(
-    const state_vector_ad_t &x,
-    const input_vector_ad_t &u,
-    const param_vector_ad_t &p,
-    state_vector_ad_t &f)
+void RocketLanding3D::systemFlowMap(const state_vector_ad_t &x,
+                                    const input_vector_ad_t &u,
+                                    const param_vector_ad_t &p,
+                                    state_vector_ad_t &f)
 {
     typedef scalar_ad_t T;
 
@@ -69,10 +68,9 @@ void RocketLanding3D::getInitializedTrajectory(Eigen::MatrixXd &X,
     }
 }
 
-void RocketLanding3D::addApplicationConstraints(
-    op::SecondOrderConeProgram &socp,
-    Eigen::MatrixXd &X0,
-    Eigen::MatrixXd &U0)
+void RocketLanding3D::addApplicationConstraints(op::SecondOrderConeProgram &socp,
+                                                Eigen::MatrixXd &X0,
+                                                Eigen::MatrixXd &U0)
 {
     const size_t K = X0.cols();
 
@@ -167,7 +165,7 @@ void RocketLanding3D::redimensionalize()
 }
 
 void RocketLanding3D::redimensionalizeTrajectory(Eigen::MatrixXd &X,
-                                Eigen::MatrixXd &U)
+                                                 Eigen::MatrixXd &U)
 {
     p.redimensionalizeTrajectory(X, U);
 }
@@ -175,6 +173,131 @@ void RocketLanding3D::redimensionalizeTrajectory(Eigen::MatrixXd &X,
 void RocketLanding3D::getNewModelParameters(param_vector_t &param)
 {
     param << p.alpha_m, p.g_I, p.J_B, p.r_T_B;
+}
+
+void RocketLanding3D::Parameters::randomizeInitialState()
+{
+    std::mt19937 eng(time(0));
+    auto dist = std::uniform_real_distribution<double>(-1., 1.);
+
+    // mass
+    x_init(0) *= 1.;
+
+    // position
+    x_init(1) *= dist(eng);
+    x_init(2) *= dist(eng);
+    x_init(3) *= 1.;
+
+    // velocity
+    x_init(4) *= dist(eng);
+    x_init(5) *= dist(eng);
+    x_init(6) *= 1. + 0.2 * dist(eng);
+
+    // orientation
+    double rx = dist(eng) * rpy_init.x();
+    double ry = dist(eng) * rpy_init.y();
+    double rz = rpy_init.z();
+    Eigen::Vector3d euler(rx, ry, rz);
+    x_init.segment(7, 4) << eulerToQuaternion(euler);
+}
+
+void RocketLanding3D::Parameters::loadFromFile()
+{
+    ParameterServer param(fmt::format("../models/config/{}.info", getModelName()));
+
+    bool randomInitialState;
+    double I_sp;
+    double m_init, m_dry;
+    Eigen::Vector3d r_init, v_init, w_init;
+    Eigen::Vector3d r_final, v_final;
+
+    param.loadMatrix("g_I", g_I);
+    param.loadMatrix("J_B", J_B);
+    param.loadMatrix("r_T_B", r_T_B);
+    param.loadScalar("m_init", m_init);
+    param.loadMatrix("r_init", r_init);
+    param.loadMatrix("v_init", v_init);
+    param.loadMatrix("rpy_init", rpy_init);
+    param.loadMatrix("w_init", w_init);
+    param.loadScalar("m_dry", m_dry);
+    param.loadMatrix("r_final", r_final);
+    param.loadMatrix("v_final", v_final);
+    param.loadScalar("final_time_guess", final_time_guess);
+    param.loadScalar("T_min", T_min);
+    param.loadScalar("T_max", T_max);
+    param.loadScalar("I_sp", I_sp);
+    param.loadScalar("gimbal_max", gimbal_max);
+    param.loadScalar("theta_max", theta_max);
+    param.loadScalar("gamma_gs", gamma_gs);
+    param.loadScalar("w_B_max", w_B_max);
+    param.loadScalar("randomInitialState", randomInitialState);
+
+    deg2rad(gimbal_max);
+    deg2rad(theta_max);
+    deg2rad(gamma_gs);
+    deg2rad(w_init);
+    deg2rad(w_B_max);
+    deg2rad(rpy_init);
+
+    alpha_m = 1. / (I_sp * fabs(g_I(2)));
+
+    x_init << m_init, r_init, v_init, eulerToQuaternion(rpy_init), w_init;
+    if (randomInitialState)
+    {
+        randomizeInitialState();
+    }
+    x_final << m_dry, r_final, v_final, 1., 0., 0., 0., 0, 0, 0;
+}
+
+void RocketLanding3D::Parameters::nondimensionalize()
+{
+    m_scale = x_init(0);
+    r_scale = x_init.segment(1, 3).norm();
+
+    alpha_m *= r_scale;
+    r_T_B /= r_scale;
+    g_I /= r_scale;
+    J_B /= (m_scale * r_scale * r_scale);
+
+    x_init(0) /= m_scale;
+    x_init.segment(1, 3) /= r_scale;
+    x_init.segment(4, 3) /= r_scale;
+
+    x_final(0) /= m_scale;
+    x_final.segment(1, 3) /= r_scale;
+    x_final.segment(4, 3) /= r_scale;
+
+    T_min /= m_scale * r_scale;
+    T_max /= m_scale * r_scale;
+}
+
+void RocketLanding3D::Parameters::redimensionalize()
+{
+    alpha_m /= r_scale;
+    r_T_B *= r_scale;
+    g_I *= r_scale;
+    J_B *= (m_scale * r_scale * r_scale);
+
+    x_init(0) *= m_scale;
+    x_init.segment(1, 3) *= r_scale;
+    x_init.segment(4, 3) *= r_scale;
+
+    x_final(0) *= m_scale;
+    x_final.segment(1, 3) *= r_scale;
+    x_final.segment(4, 3) *= r_scale;
+
+    T_min *= m_scale * r_scale;
+    T_max *= m_scale * r_scale;
+}
+
+void RocketLanding3D::Parameters::redimensionalizeTrajectory(Eigen::MatrixXd &X, Eigen::MatrixXd &U)
+{
+    const size_t K = X.cols();
+
+    X.row(0) *= m_scale;
+    X.block(1, 0, 6, K) *= r_scale;
+
+    U *= m_scale * r_scale;
 }
 
 } // namespace rocket3d
