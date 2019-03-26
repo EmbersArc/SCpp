d3562fcf7e038468c2fe6b2c1631729e2ea177eb
Sven Niederberger <svenn@ethz.ch>
2019-03-26 12:17:41 +0100

    add cartpole

--- SCpp/include/activeModel.hpp
+++ SCpp/include/activeModel.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// #include "models/rocketLanding2d.hpp"
+// #include "rocketLanding2d.hpp"
 // using Model = rocket2d::RocketLanding2D;
 
 #include "rocketLanding3d.hpp"
@@ -8,3 +8,6 @@ using Model = rocket3d::RocketLanding3D;
 
 // #include "crewDragon.hpp"
 // using Model = crewdragon::CrewDragon;
+
+// #include "cartpole.hpp"
+// using Model = cartpole::Cartpole;
