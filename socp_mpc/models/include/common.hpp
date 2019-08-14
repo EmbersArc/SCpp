2473af3679222f4af76f8c60ea71e06d5b259369
Sven Niederberger <svenn@ethz.ch>
2019-08-14 18:24:48 +0200

    new functions

--- socp_mpc/models/include/common.hpp
+++ socp_mpc/models/include/common.hpp
@@ -10,13 +10,9 @@ void deg2rad(T &deg)
 }
 
 template <typename T>
-Eigen::Quaternion<T> eulerToQuaternion(const Eigen::Matrix<T, 3, 1> &eta)
+void rad2deg(T &rad)
 {
-    Eigen::Quaternion<T> q;
-    q = Eigen::AngleAxis<T>(eta.x(), Eigen::Matrix<T, 3, 1>::UnitX()) *
-        Eigen::AngleAxis<T>(eta.y(), Eigen::Matrix<T, 3, 1>::UnitY()) *
-        Eigen::AngleAxis<T>(eta.z(), Eigen::Matrix<T, 3, 1>::UnitZ());
-    return q;
+    rad *= 180. / M_PI;
 }
 
 template <typename T>
@@ -25,6 +21,49 @@ Eigen::Matrix<T, 4, 1> quaternionToVector(const Eigen::Quaternion<T> &q)
     Eigen::Matrix<T, 4, 1> q_vec;
     q_vec << q.w(), q.vec();
     return q_vec;
+}
+
+// sequence x-y'-z'' is XYZ = Z''Y'X
+template <typename T>
+Eigen::Quaternion<T> eulerToQuaternion(const Eigen::Matrix<T, 3, 1> &eta)
+{
+    Eigen::Quaternion<T> q;
+    q = Eigen::AngleAxis<T>(eta.x(), Eigen::Matrix<T, 3, 1>::UnitX()) *
+        Eigen::AngleAxis<T>(eta.y(), Eigen::Matrix<T, 3, 1>::UnitY()) *
+        Eigen::AngleAxis<T>(eta.z(), Eigen::Matrix<T, 3, 1>::UnitZ());
+
+    return q;
+}
+
+// sequence x-y-z is ZYX
+template <typename T>
+Eigen::Quaternion<T> eulerToQuaternionExtrinsic(const Eigen::Matrix<T, 3, 1> &eta)
+{
+    Eigen::Quaternion<T> q;
+    q = Eigen::AngleAxis<T>(eta.z(), Eigen::Matrix<T, 3, 1>::UnitZ()) *
+        Eigen::AngleAxis<T>(eta.y(), Eigen::Matrix<T, 3, 1>::UnitY()) *
+        Eigen::AngleAxis<T>(eta.x(), Eigen::Matrix<T, 3, 1>::UnitX());
+    return q;
+}
+
+template <typename T>
+Eigen::Matrix<T, 3, 1> quaternionToEuler(const Eigen::Quaternion<T> &q)
+{
+    const Eigen::Matrix<T, 3, 3> R = q.toRotationMatrix();
+    const T phi = atan2(-R(1, 2), R(2, 2));
+    const T theta = asin(R(0, 2));
+    const T psi = atan2(-R(0, 1), R(0, 0));
+    return Eigen::Matrix<T, 3, 1>(phi, theta, psi);
+}
+
+template <typename T>
+Eigen::Matrix<T, 3, 1> quaternionToEulerExtrinsic(const Eigen::Quaternion<T> &q)
+{
+    const Eigen::Matrix<T, 3, 3> R = q.toRotationMatrix();
+    const T phi = atan2(R(1, 0), R(0, 0));
+    const T theta = asin(-R(2, 0));
+    const T psi = atan2(R(2, 1), R(2, 2));
+    return Eigen::Matrix<T, 3, 1>(psi, theta, phi);
 }
 
 template <typename T>
@@ -37,27 +76,6 @@ template <typename T>
 Eigen::Quaternion<T> vectorToQuaternion(const Eigen::Matrix<T, 4, 1> &v)
 {
     return Eigen::Quaternion<T>(v(3), v(0), v(1), v(2));
-}
-
-template <typename T>
-Eigen::Matrix<T, 3, 1> quaternionToEulerRPY(const Eigen::Quaternion<T> &q)
-{
-    const Eigen::Matrix<T, 3, 3> R = q.toRotationMatrix();
-    const T phi = atan2(-R(1, 2), R(2, 2));
-    const T theta = asin(R(0, 2));
-    const T psi = atan2(-R(0, 1), R(0, 0));
-
-    return Eigen::Matrix<T, 3, 1>(phi, theta, psi);
-}
-
-template <typename T>
-Eigen::Matrix<T, 3, 1> quaternionToEulerYPR(const Eigen::Quaternion<T> &q)
-{
-    const Eigen::Matrix<T, 3, 3> R = q.toRotationMatrix();
-    const T phi = atan2(R(2, 1), R(2, 2));
-    const T theta = asin(-R(2, 0));
-    const T psi = atan2(R(1, 0), R(0, 0));
-    return Eigen::Matrix<T, 3, 1>(phi, theta, psi);
 }
 
 template <typename T>
@@ -97,21 +115,4 @@ Eigen::Matrix<T, 3, 3> omegaMatrixReduced(const Eigen::Matrix<T, 3, 1> &q)
         -q(1), q(0), qw;
 
     return omegaMatrix;
-}
-
-template <typename T>
-Eigen::Matrix<T, 3, 3> EulerRotationMatrix(const Eigen::Matrix<T, 3, 1> &eta)
-{
-    const T phi = eta(0);
-    const T theta = eta(1);
-    const T psi = eta(2);
-
-    Eigen::Matrix<T, 3, 3> R;
-    R.row(0) << cos(theta) * cos(psi), -cos(theta) * sin(psi), sin(theta);
-    R.row(1) << sin(phi) * sin(theta) * cos(psi) + cos(phi) * sin(psi),
-        cos(phi) * cos(psi) - sin(phi) * sin(theta) * sin(psi), -sin(phi) * cos(theta);
-    R.row(2) << -cos(phi) * sin(theta) * cos(psi) + sin(phi) * sin(psi),
-        sin(phi) * cos(psi) + cos(phi) * sin(theta) * sin(psi), cos(phi) * cos(theta);
-
-    return R;
 }
