06b73839a5c8049c1b0ce9815bd7eef381abfa80
Sven Niederberger <svenn@ethz.ch>
2019-10-07 22:50:47 +0200

    .

--- scpp_core/include/activeModel.hpp
+++ scpp_core/include/activeModel.hpp
@@ -2,22 +2,10 @@
 
 // --- SC ---
 
-// #include "rocketLanding2d.hpp"
-// using Model = rocket2d::RocketLanding2D;
-
-#include "starship.hpp"
-using Model = starship::Starship;
-
-// #include "rocket3d.hpp"
-// using Model = rocket3d::Rocket3D;
-
-// #include "crewDragon.hpp"
-// using Model = crewdragon::CrewDragon;
-
-// #include "cartpole.hpp"
-// using Model = cartpole::Cartpole;
+// #include "starship.hpp"
+// using Model = starship::Starship;
 
 // --- MPC ---
 
-// #include "rocketHover.hpp"
-// using Model = rocketHover::RocketHover;
+#include "rocketHover.hpp"
+using Model = rocketHover::RocketHover;
