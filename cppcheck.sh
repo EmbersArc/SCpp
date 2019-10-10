fa591a55f451b7afa988b8c58cbe34b0c17daba7
Sven Niederberger <svenn@ethz.ch>
2019-10-10 16:43:50 +0200

    update for new folder structure

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,1 +1,1 @@
-clear && cppcheck src socp_mpc -I socp_mpc/socp_interface/include -I socp_mpc/include -i socp_mpc/socp_interface/solvers --enable=all --inconclusive --language=c++
+clear && cppcheck src scpp_core scpp_models -I scpp_core/include -I scpp_models/include -i lib/socp_interface --enable=all --inconclusive --language=c++
