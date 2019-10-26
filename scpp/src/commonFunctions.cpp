3042bf230841cfd95c01c722f44851b62ff604a5
Sven Niederberger <svenn@ethz.ch>
2019-10-26 11:04:34 +0200

    factor out common functions

---
+++ scpp/src/commonFunctions.cpp
@@ -0,0 +1,21 @@
+#include "commonFunctions.hpp"
+
+namespace scpp
+{
+
+Model::input_vector_t interpolatedInput(const Model::input_vector_v_t &U, double t,
+                                        double total_time, bool constant)
+{
+    const size_t K = U.size();
+    const double time_step = total_time / (K - 1);
+    const size_t i = std::min(size_t(t / time_step), K - 2);
+    const Model::input_vector_t u0 = U.at(i);
+    const Model::input_vector_t u1 = constant ? U.at(i + 1) : u0;
+
+    const double t_intermediate = std::fmod(t, time_step) / time_step;
+    const Model::input_vector_t u = u0 + (u1 - u0) * t_intermediate;
+
+    return u;
+}
+
+} // namespace scpp
