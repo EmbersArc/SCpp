88a52e0a3acadc0f3a765f24ce9595d6e6623145
Sven Niederberger <svenn@ethz.ch>
2019-10-26 11:04:15 +0200

    clean up a bit

--- scpp_core/include/activeModel.hpp
+++ scpp_core/include/activeModel.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
-// --- SC ---
-
 // #include "starship.hpp"
 // using Model = scpp::models::Starship;
-
-// --- MPC ---
 
 #include "rocketHover.hpp"
 using Model = scpp::models::RocketHover;
 
+// ---
 
 using DiscretizationData = Model::DiscretizationData;
 using TrajectoryData = Model::TrajectoryData;
