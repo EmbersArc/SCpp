54341117b2d5cf0dc71d2bccd549e94bc1ae3e62
Sven Niederberger <svenn@ethz.ch>
2019-10-28 20:31:30 +0100

    fix argument name

--- scpp/include/commonFunctions.hpp
+++ scpp/include/commonFunctions.hpp
@@ -6,6 +6,6 @@ namespace scpp
 {
 
 Model::input_vector_t interpolatedInput(const Model::input_vector_v_t &U, double t,
-                                        double total_time, bool constant);
+                                        double total_time, bool first_order_hold);
                                         
 } // namespace scpp
