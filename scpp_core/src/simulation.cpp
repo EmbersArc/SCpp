36ff1a199cd2a50649bfaa091a1dcc567abb450b
Sven Niederberger <svenn@ethz.ch>
2019-10-31 19:21:18 +0100

    more accuracy

--- scpp_core/src/simulation.cpp
+++ scpp_core/src/simulation.cpp
@@ -7,7 +7,8 @@
 namespace scpp
 {
 
-ODE::ODE(Model::ptr_t model, double dt, const Model::input_vector_t &u0, const Model::input_vector_t &u1) : model(model), u0(u0), u1(u1), dt(dt) {}
+ODE::ODE(Model::ptr_t model, double dt, const Model::input_vector_t &u0, const Model::input_vector_t &u1)
+    : model(model), u0(u0), u1(u1), dt(dt) {}
 
 void ODE::operator()(const Model::state_vector_t &f, Model::state_vector_t &dfdt, const double t)
 {
@@ -21,11 +22,11 @@ void simulate(Model::ptr_t model, double dt,
               Model::state_vector_t &x)
 {
     using namespace boost::numeric::odeint;
-    runge_kutta4<Model::state_vector_t, double, Model::state_vector_t, double, vector_space_algebra> stepper;
+    runge_kutta_fehlberg78<Model::state_vector_t, double, Model::state_vector_t, double, vector_space_algebra> stepper;
 
     ODE ode(model, dt, u0, u1);
 
-    integrate_adaptive(stepper, ode, x, 0., dt, dt / 10.);
+    integrate_adaptive(stepper, ode, x, 0., dt, dt / 20.);
 }
 
 } // namespace scpp
