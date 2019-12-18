252163f0120d4356d35b7e8465976978191e74d0
Sven Niederberger <s-niederberger@hotmail.com>
2019-12-18 15:25:45 +0100

    remove commented out code

--- scpp/src/SC_oneshot.cpp
+++ scpp/src/SC_oneshot.cpp
@@ -64,38 +64,4 @@ int main()
         }
     }
     fmt::print("{:<{}}{:.2f}ms\n", "Time, solution files:", 50, toc(timer));
-
-    // verify by integrating
-    // const auto &td = all_td.back();
-    // Model::state_vector_t x = td.X.front();
-    // const double timestep = td.t / (td.X.size() - 1);
-    // const bool first_order_hold = td.interpolatedInput();
-
-    // for (size_t k = 0; k < td.X.size() - 1; k++)
-    // {
-    //     scpp::simulate(model, timestep, td.U[k], first_order_hold ? td.U[k + 1] : td.U[k], x);
-    // }
-
-    // double full_error = (x - td.X.back()).lpNorm<1>();
-
-    // fmt::print("\nVerifying solution...\n");
-    // fmt::print("{:<{}}{:.5f}\n", "Final deviation:", 50, full_error);
-
-    // save accelerations:
-    // {
-    //     const fs::path iterationPath = outputPath / "x";
-
-    //     if (not fs::exists(iterationPath) and not fs::create_directories(iterationPath))
-    //     {
-    //         throw std::runtime_error("Could not create output directory!");
-    //     }
-
-    //     {
-    //         std::ofstream f(iterationPath / "acc_passenger_b.txt");
-    //         for (auto &pv : getAccelerationRotatingFrame(all_td.back(), Eigen::Vector3d(0., 0., 0.), 9.81))
-    //         {
-    //             f << pv.transpose().format(CSVFormat) << "\n";
-    //         }
-    //     }
-    // }
 }
