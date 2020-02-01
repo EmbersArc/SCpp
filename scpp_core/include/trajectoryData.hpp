f0e9ba8ddc5c69f4377f1858cf6454ab231589a9
Sven Niederberger <s-niederberger@outlook.com>
2020-02-01 23:12:54 +0100

    add helper methods

--- scpp_core/include/trajectoryData.hpp
+++ scpp_core/include/trajectoryData.hpp
@@ -16,6 +16,9 @@ struct TrajectoryData
 
     bool interpolatedInput() const;
 
+    typename Model::input_vector_t inputAtTime(double t) const;
+    typename Model::state_vector_t approxStateAtTime(double t) const;
+
     size_t n_X() const;
     size_t n_U() const;
 };
@@ -32,6 +35,34 @@ template <class Model>
 bool TrajectoryData<Model>::interpolatedInput() const
 {
     return U.size() == X.size();
+}
+
+template <class Model>
+typename Model::input_vector_t TrajectoryData<Model>::inputAtTime(double t) const
+{
+    t = std::clamp(t, 0., this->t);
+    const double dt = this->t / (n_X() - 1);
+    double interpolate_value = std::fmod(t, dt) / dt;
+    const size_t i = t / dt;
+
+    const typename Model::input_vector_t u0 = U.at(i);
+    const typename Model::input_vector_t u1 = interpolatedInput() ? U.at(i + 1) : U.at(i);
+
+    return u0 + interpolate_value * (u1 - u0);
+}
+
+template <class Model>
+typename Model::state_vector_t TrajectoryData<Model>::approxStateAtTime(double t) const
+{
+    t = std::clamp(t, 0., this->t);
+    const double dt = this->t / (n_X() - 1);
+    double interpolate_value = std::fmod(t, dt) / dt;
+    const size_t i = t / dt;
+
+    const typename Model::state_vector_t x0 = X.at(i);
+    const typename Model::state_vector_t x1 = X.at(i + 1);
+
+    return x0 + interpolate_value * (x1 - x0);
 }
 
 template <class Model>
